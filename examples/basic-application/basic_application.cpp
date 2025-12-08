050e89af8d82c3264ff04cd10eb2c2c3416dc97e
Oleksandr <apivovarov@quicklogic.com>
2025-12-08 12:35:16 +0200

    checkpoint

--- examples/basic-application/basic_application.cpp
+++ examples/basic-application/basic_application.cpp
@@ -550,10 +550,10 @@ void draw_png_example(ezgl::renderer *g)
   ezgl::surface *png_surface = ezgl::renderer::load_png("small_image.png");
   g->draw_surface(png_surface, {50, 200});
   ezgl::renderer::free_surface(png_surface);
+#endif
   g->set_font_size(10);
   g->set_color(ezgl::BLACK);
   g->draw_text ({50, 225}, "draw_surface", 200, DBL_MAX);
-#endif
 }
 
 /**
