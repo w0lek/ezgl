b2acf7a1c51b8415bb22e28640164c0d9ca9e3c7
Oleksandr <apivovarov@quicklogic.com>
2026-06-23 16:29:42 +0300

    Document zoom_in/zoom_out point overloads take widget (screen) coordinates

--- include/ezgl/control.hpp
+++ include/ezgl/control.hpp
@@ -40,12 +40,12 @@ void zoom_in(canvas *cnv, double zoom_factor);
 void zoom_out(canvas *cnv, double zoom_factor);
 
 /**
- * Zoom in on a specific point in the DrawingAreaWidget / RhiCanvasWidget.
+ * Zoom in on a specific point, given in widget (screen) coordinates.
  */
 void zoom_in(canvas *cnv, point2d zoom_point, double zoom_factor);
 
 /**
- * Zoom out from a specific point in the DrawingAreaWidget / RhiCanvasWidget.
+ * Zoom out from a specific point, given in widget (screen) coordinates.
  */
 void zoom_out(canvas *cnv, point2d zoom_point, double zoom_factor);
 
